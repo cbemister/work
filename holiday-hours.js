afd366e8e5752e729b688cf5ea76db072ca93817
Chris Bemister <29780975+cbemister@users.noreply.github.com>
2021-12-14 14:15:28 -0700

    Update holiday-hours.js
    Cleaned up variable names

--- holiday-hours.js
+++ holiday-hours.js
@@ -102,42 +102,6 @@ var holidayHours = {
 				'time': 'Closed'
 			},
 		}
-	},
-	'Parkland Chrysler Dodge Jeep Ram': {
-		'Sales': {
-			'12/23/2021': {
-				'label': 'December 23rd',
-				'time': 'Closed'
-			},
-			'12/24/2021': {
-				'label': 'Christmas Eve',
-				'time': 'Closed'
-			},
-			'12/25/2021': {
-				'label': 'Christmas Day',
-				'time': 'Closed'
-			},
-			'12/26/2021': {
-				'label': 'Boxing Day',
-				'time': 'Closed'
-			},
-			'12/31/2021': {
-				'label': 'New Years Eve',
-				'time': 'Closed'
-			},
-			'1/1/2022': {
-				'label': 'New Years Day',
-				'time': 'Closed'
-			},
-			'1/2/2022': {
-				'label': 'January 2nd',
-				'time': 'Closed'
-			},
-			'1/3/2022': {
-				'label': 'January 3rd',
-				'time': 'Closed'
-			}
-		}
 	}
 };
 
@@ -152,13 +116,13 @@ var currentDepartmentHours = holidayHours[currentDealership][currentDepartment];
 var holidayKeys = Object.keys(currentDepartmentHours);
 
 // INIT CONTAINER VARS
-var holidayHoursNotice = [],
-	holidayHoursBody = [],
-	holidayHoursHeader = '',
-	holidayHoursFooter = '';
-// SIDE BAR MARK-UP
-var holidayHoursOpen = '<table class="hours hours--simple hours--holiday"><caption class="seo-hidden">Dealership hours of operation</caption><tbody>';
-var holidayHoursClose = '</tbody></table>';
+var sidebarNoticeHTML = [],
+	headerMessageText = '',
+	footerMessageText = '';
+// PARTIAL SIDE BAR MARK-UP
+var noticeBody = [],
+	noticeOpeningDiv = '<table class="hours hours--simple hours--holiday"><caption class="seo-hidden">Dealership hours of operation</caption><tbody>',
+	noticeClosingDiv = '</tbody></table>';
 
 // SET TODAY'S DATE
 var today = new Date();
@@ -185,44 +149,44 @@ jQuery(document).ready(function () {
 
 	// CREATE HOLIDAY NOTICE ON PAGE LOAD
 	createSideBarNotice(currentDepartmentHours);
-	setHeaderFooterHours(currentDepartmentHours);
+	setHeaderFooterText(currentDepartmentHours);
 
 	if (holidayHoursEnabled && currentDepartmentHours) {
 		// SET HOLIDAY HOURS IN SIDEBAR
-		jQuery('.sidebar__widget.sidebar-hours-of-operation-widget').append('<div id="hours--holiday">' + holidayHoursNotice + '</div>');
+		jQuery('.sidebar__widget.sidebar-hours-of-operation-widget').append('<div id="hours--holiday">' + sidebarNoticeHTML + '</div>');
 
 		jQuery('.footer__content h4.footer__title').text('Holiday Hours');
-		setHeaderFooterHours(currentDepartmentHours); // IF CONDITION IS MET
+		setHeaderFooterText(currentDepartmentHours); // IF CONDITION IS MET
 
 
 		// SET EVENT LISTENER ON SELECT
 		jQuery('.hours-select').change(function () {
 			currentDepartment = this.options[this.selectedIndex].text // GET DEPARTEMENT LABEL
 			currentDepartmentHours = holidayHours[currentDealership][currentDepartment]; // GET DEALERSHIP HOURS
-			holidayHoursBody = []; // RESET CURRENT HOLIDAY HOURS
+			noticeBody = []; // RESET CURRENT HOLIDAY HOURS
 			// SET HOLIDAY HOURS BASED ON SELECTION
 			createSideBarNotice(currentDepartmentHours);
-			setHeaderFooterHours(currentDepartmentHours);
-			jQuery('#hours--holiday').html(holidayHoursNotice); // REPLACE CURRENT HOLIDAY HOURS
+			setHeaderFooterText(currentDepartmentHours);
+			jQuery('#hours--holiday').html(sidebarNoticeHTML); // REPLACE CURRENT HOLIDAY HOURS
 		});
 	}
 
 });
 
-function setHeaderFooterHours(selectedHours) {
+function setHeaderFooterText(selectedHours) {
 	if (selectedHours[today]) {
-		var holidayHoursHeader = selectedHours[today]['time'] === 'Closed' ? 'Closed Today For The Holiday' : selectedHours[today]['time']
+		headerMessageText = selectedHours[today]['time'] === 'Closed' ? 'Closed Today For The Holiday' : selectedHours[today]['time']
 
-		holidayHoursFooter = `
+		footerMessageText = `
 		<tr class="hours__row">
 			<th scope="row" class="hours__day hours__day--en"><strong>${selectedHours[today]['label']}</strong></th>
 			<td class="hours__time">${selectedHours[today]['time']}</td>
 		</tr>`;
 		// SET HOLIDAY HOURS IN HEADER
-		jQuery('.header-popout label.header-popout__label').text(holidayHoursHeader);
+		jQuery('.header-popout label.header-popout__label').text(headerMessageText);
 		jQuery('.header-popout label.header-popout__label').css('cursor', 'default');
 		// SET HOLIDAY HOURS IN FOOTER
-		jQuery('.footer__content table.hours.hours--simple tbody').html(holidayHoursFooter);
+		jQuery('.footer__content table.hours.hours--simple tbody').html(footerMessageText);
 	}
 }
 
@@ -231,7 +195,7 @@ function createSideBarNotice(selectedHours) {
 		var currentHours = new Date(key);
 
 		if (currentDate <= currentHours && (currentDate <= endDate)) {
-			holidayHoursBody.push(`
+			noticeBody.push(`
 			<tr class="hours__row">
 				<th scope="row" class="hours__day hours__day--en"><strong>${selectedHours[key]['label']}</strong></th>
 				<td class="hours__time">${selectedHours[key]['time']}</td>
@@ -242,5 +206,5 @@ function createSideBarNotice(selectedHours) {
 	});
 
 	// Build Holiday Hours Notice Mark-Up
-	holidayHoursNotice = '<h4 class="sidebar-title hours--holiday mt-md">Holiday Hours <i aria-hidden="true" class="fas fa-clock"></i></h4>' + holidayHoursOpen + holidayHoursBody.join('') + holidayHoursClose;
+	sidebarNoticeHTML = '<h4 class="sidebar-title hours--holiday mt-md">Holiday Hours <i aria-hidden="true" class="fas fa-clock"></i></h4>' + noticeOpeningDiv + noticeBody.join('') + noticeClosingDiv;
 }
