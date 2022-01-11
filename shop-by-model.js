13f2b286183b01bb522c31c2e69fce63e22ba3f2
Chris Bemister <29780975+cbemister@users.noreply.github.com>
2022-01-11 11:24:19 -0700

    Create shop-by-model.js

---
+++ shop-by-model.js
@@ -0,0 +1,101 @@
+var navFragmentContent = `<div id="custom-fragment-models">
+	<div class="model-container">
+		<ul class="model-links">
+			<li>
+				<h3 class="model-heading">Chrysler Models</h3>
+				<a href="/vehicles/new/?st=asking_price,asc&view=grid&sc=new&mk=Chrysler&md=Pacifica" class="chrysler pacifica">
+					<p>Pacifica</p>
+				</a>
+			</li>
+			<li>
+				<h3 class="model-heading">Dodge Models</h3>
+				<a href="/vehicles/new/?st=asking_price,asc&view=grid&sc=new&mk=Dodge&md=Challenger" class="dodge challenger">
+					<p>Challenger</p>
+				</a>
+			</li>
+			<li>
+				<a href="/vehicles/new/?st=asking_price,asc&view=grid&sc=new&mk=Dodge&md=Charger" class="dodge charger">
+					<p>Charger</p>
+				</a>
+			</li>
+			<li>
+				<a href="/vehicles/new/?st=asking_price,asc&view=grid&sc=new&mk=Dodge&md=Durango" class="dodge durango">
+					<p>Durango</p>
+				</a>
+			</li>
+			<li>
+				<a href="/vehicles/new/?st=asking_price,asc&view=grid&sc=new&mk=Dodge&md=Journey" class="dodge journey">
+					<p>Journey</p>
+				</a>
+			</li>
+			<li>
+				<h3 class="model-heading">Jeep Models</h3>
+				<a href="/vehicles/new/?st=price,asc&view=grid&sc=new&mk=Jeep&md=Wagoneer" class="jeep wagoneer">
+					<p>Wagoneer</p>
+				</a>
+			</li>
+			<li>
+				<a href="/vehicles/new/?st=price,asc&view=grid&sc=new&mk=Jeep&md=GrandWagoneer" class="jeep grand-wagoneer">
+					<p>Grand Wagoneer</p>
+				</a>
+			</li>
+			<li>
+				<a href="https://www.parklanddodge.com/vehicles/new/?view=grid&sc=new&st=asking_price,asc&mk=Jeep&md=Grand Cherokee L" class="jeep grand-cherokee-l">
+					<p>Grand Cherokee L</p>
+				</a>
+			</li>
+			<li>
+				<a href="/vehicles/new/?st=asking_price,asc&view=grid&sc=new&mk=Jeep&md=Cherokee" class="jeep cherokee">
+					<p>Cherokee</p>
+				</a>
+			</li>
+			<li>
+				<a href="/vehicles/new/?st=asking_price,asc&view=grid&sc=new&mk=Jeep&md=Compass" class="jeep compass">
+					<p>Compass</p>
+				</a>
+			</li>
+			<li>
+				<a href="/vehicles/new/?st=asking_price,asc&view=grid&sc=new&mk=Jeep&md=Gladiator" class="jeep gladiator">
+					<p>Gladiator</p>
+				</a>
+			</li>
+			<li class="menu-spacer"></li>
+			<li>
+				<a href="https://www.parklanddodge.com/vehicles/new/?st=asking_price,asc&view=grid&sc=new&mk=Jeep&md=Grand Cherokee" class="jeep grand-cherokee">
+					<p>Grand Cherokee</p>
+				</a>
+			</li>
+			<li>
+				<a href="/vehicles/new/?st=asking_price,asc&view=grid&sc=new&mk=Jeep&md=Renegade" class="jeep renegade">
+					<p>Renegade</p>
+				</a>
+			</li>
+			<li class="column-end">
+				<a href="/vehicles/new/?st=asking_price,asc&view=grid&sc=new&mk=Jeep&md=Wrangler,Wrangler Unlimited" class="jeep wrangler">
+					<p>Wrangler</p>
+				</a>
+			</li>
+			<li>
+				<h3 class="model-heading">RAM Trucks</h3>
+				<a href="/vehicles/new/?st=asking_price,asc&view=grid&sc=new&mk=Ram&md=1500" class="ram ram-1500">
+					<p>1500</p>
+				</a>
+			</li>
+			<li>
+				<a href="/vehicles/new/?st=asking_price,asc&view=grid&sc=new&mk=Ram&md=1500 Classic,1500Classic" class="ram ram-1500-classic">
+					<p>1500 Classic</p>
+				</a>
+			</li>
+			<li>
+				<a href="/vehicles/new/?st=asking_price,asc&view=grid&sc=new&mk=Ram&md=2500" class="ram ram-2500">
+					<p>2500</p>
+				</a>
+			</li>
+			<li>
+				<a href="/vehicles/new/?st=asking_price,asc&view=grid&sc=new&mk=Ram&md=3500" class="ram ram-3500">
+					<p>3500</p>
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>`;
