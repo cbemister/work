ca47ae6fe51e63954ea949b67299e6c455c28541
Chris Bemister <29780975+cbemister@users.noreply.github.com>
2022-06-08 10:11:40 -0600

    Create javascript-app-template.js

---
+++ javascript-app-template.js
@@ -0,0 +1,25 @@
+
+/*
+ * NAME OF SCRIPT
+ */
+
+var APPNAME = APPNAME || {};
+
+(function (context) {
+
+// LIST VARIABLES HERE
+
+// LIST METHODS HERE
+
+// INIT FUNCTION
+  
+  function init(){
+    
+  }
+
+  document.addEventListener("DOMContentLoaded", init);
+  if (document.readyState !== "loading") {
+   init()
+  }
+
+})(APPNAME);
