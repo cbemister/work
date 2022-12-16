0ae00616d8b08024bb1842914b9bca798d1a0bd1
cbemisterAC <cbemister@autocan.ca>
2022-12-16 12:18:53 -0700

    Minor updates

--- ~ FUNCTIONALITY/SRP_mutation_observer.js
+++ ~ FUNCTIONALITY/SRP_mutation_observer.js
@@ -1,41 +1,41 @@
 $(document).ready(function () {
-	const observer = new MutationObserver(mCallback);
+ const observer = new MutationObserver(mCallback);
 
-	var listElm = document.querySelector('.srp-wrapper');
+ var listElm = document.querySelector('.srp-wrapper');
 
-	// to look for changes
-	const config = {
-		attributes: false,
-		childList: true,
-		subtree: true,
-	};
+ // to look for changes
+ const config = {
+  attributes: false,
+  childList: true,
+  subtree: true,
+ };
 
-	observer.observe(listElm, config)
+ observer.observe(listElm, config)
 
-	// add click listener to video cta
-	// $("[convertus-data-id='vdp__video-trigger']").on("click", observer.observe(photoGallery, config));
+ // add click listener to video cta
+ // $("[convertus-data-id='vdp__video-trigger']").on("click", observer.observe(photoGallery, config));
 
-	function mCallback(mutations) {
+ function mCallback(mutations) {
 
-		for (let mutation of mutations) {
+  for (let mutation of mutations) {
 
-			if (mutation?.addedNodes[0]?.classList && mutation?.addedNodes[0].classList[0] === 'vehicle-card' || mutation?.addedNodes[0]?.classList && mutation?.addedNodes[0].classList[0] === 'mb-lg') {
-				jQuery('.mb-lg.grid-view.col:not(.modified)').each(function () {
-	
-					if (!jQuery(this).hasClass('modified')) {
-						console.log(jQuery(this).find('h4.vehicle-card__title').text());
-						jQuery(this).addClass('modified');
-					}
-				});
-			}
+   if (mutation ? mutation.addedNodes[0] ? mutation.classList && mutation ? mutation.addedNodes[0].classList[0] === 'vehicle-card' || mutation ? mutation.addedNodes[0] ? mutation.classList && mutation ? .addedNodes[0].classList[0] === 'mb-lg') {
+    jQuery('.mb-lg.grid-view.col:not(.modified)').each(function () {
 
-			// console.log(mutation.addedNodes[0] ? mutation.addedNodes[0] : false);
-			// let photoGalleryLoaded = mutation.addedNodes[0] ? .lastElementChild ? ._prevClass === "photo-gallery__main photo-gallery__main--video" ? true : false;
-			// if (photoGalleryLoaded) {
-			// 	loadYoutubeVideo();
-			// 	break;
-			// }
-		}
-	}
+     if (!jQuery(this).hasClass('modified')) {
+      console.log(jQuery(this).find('h4.vehicle-card__title').text());
+      jQuery(this).addClass('modified');
+     }
+    });
+   }
+
+   // console.log(mutation.addedNodes[0] ? mutation.addedNodes[0] : false);
+   // let photoGalleryLoaded = mutation.addedNodes[0] ? .lastElementChild ? ._prevClass === "photo-gallery__main photo-gallery__main--video" ? true : false;
+   // if (photoGalleryLoaded) {
+   // 	loadYoutubeVideo();
+   // 	break;
+   // }
+  }
+ }
 
 });
